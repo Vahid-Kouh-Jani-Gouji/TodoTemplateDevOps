f5bcf8a6bb72dbc2de58b0dd8af21890a5dfd385
Yaser Moradi <ysmoradi@outlook.com>
2022-10-28 22:31:46 +0330

    cd

---
+++ src/Server/Api/Data/AppDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace TodoTemplateDevOps.Server.Api.Data;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+        optionsBuilder.UseSqlServer(args[1]);
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
